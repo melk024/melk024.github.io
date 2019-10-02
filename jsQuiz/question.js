6debbbe2dc70922667d7510cd44416863b381f10
Mel Kerber <mkerber1@andrew.cmu.edu>
2019-10-02 11:16:31 -0400

    Update question

--- jsQuiz/question.js
+++ jsQuiz/question.js
@@ -33,6 +33,11 @@ class Quiz {
     	}
 		this.questionIndex++;
 	}
+	
+	isEnded() {
+		return this.questionIndex === this.questions.length;
+	}
+
 }
 
 function guess(id, guess) {
@@ -45,19 +50,30 @@ function guess(id, guess) {
 
 function populate() {
 
-   	 var element = document.getElementById("question");
-   	 element.innerHTML = quiz.getQuestion().text;
- 
-   	 // show options
-   	 var choices = quiz.getQuestion().choices;
-   	 for(var i = 0; i < choices.length; i++) {
-   		 var element = document.getElementById("choice" + i);
-   		 element.innerHTML = choices[i];
-   		 guess("btn" + i, choices[i]);
-   	 }
- 
-    
+	if(quiz.isEnded()){
+		showScores();
+	}
+	else {
+		 var element = document.getElementById("question");
+		 element.innerHTML = quiz.getQuestion().text;
+	 
+		 // show options
+		 var choices = quiz.getQuestion().choices;
+		 for(var i = 0; i < choices.length; i++) {
+			 var element = document.getElementById("choice" + i);
+			 element.innerHTML = choices[i];
+			 guess("btn" + i, choices[i]);
+		}
+	}
 };
+
+function showScores() {
+	var gameOverHTML = "<h1>Result</h1>";
+	gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
+	var element = document.getElementById("quiz");
+	element.innerHTML = gameOverHTML;
+};
+
 
 var quiz = new Quiz(questions);
 populate();
