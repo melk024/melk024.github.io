cbbf5b01911f65bc81d29fad64dd0f0a8a500157
Mel Kerber <mkerber1@andrew.cmu.edu>
2019-10-30 19:17:57 -0400

    added lab work from week 10, item auto-update script for homework 6B

--- homework_5/script.js
+++ homework_5/script.js
@@ -1,18 +1,13 @@
 //if some products already exist, maintain the list between pages
 //else, create a new empty products list
 
-var products = [];
-
 /*function cartAdd(){
 	items++;
 	localStorage.setItem("cartitems", items);
-	document.getElementById("lblCartCount").innerHTML = localStorage.getItem("cartitems");
+	 = localStorage.getItem("cartitems");
 }
 
-function onLoad(){
-	items = localStorage.getItem("cartitems");
-	document.getElementById("lblCartCount").innerHTML = items;
-}*/
+*/
 
 function Bun(flavor, glaze, number, pic){
 	this.flavor = flavor;
@@ -47,5 +42,143 @@ function makeItem(){
 }
 
 function cartAdd(item){	
+	var products = JSON.parse(localStorage.getItem("productsList"));
+	
+	if(products === null){
+		products = [];
+	}
+	
 	products.push(item);
+	localStorage.setItem("productsList", JSON.stringify(products));
+	
+	localStorage.setItem("cartNumber", products.length);
+	document.getElementById("lblCartCount").innerHTML = localStorage.getItem("cartNumber");
+	
+	//take item
+	//create cart item
+	//appendChild to mini cart div	
+	
+	var cartItem = document.createElement("div");
+	cartItem.className = "smallItem";
+			
+	var cartPic = document.createElement("div");
+	cartPic.className = "itemPic";
+	cartPic.innerHTML = item.pic;
+		
+	var cartDesc = document.createElement("div");
+	cartDesc.className = "itemDesc";
+	cartDesc.innerHTML = item.flavor + "<br><br> Glaze: " + item.glaze + "<br> Quantity: " + item.number;
+			
+	cartItem.appendChild(cartPic);
+	cartItem.appendChild(cartDesc);
+			
+	document.getElementById("cart").appendChild(cartItem);
+}
+
+function onLoad() {
+	var count = localStorage.getItem("cartNumber");
+	if(count === null){
+		count = 0;
+	}
+	document.getElementById("lblCartCount").innerHTML = count;
+	
+	var products = JSON.parse(localStorage.getItem("productsList"));
+	
+	if(products === null){		
+		var cartLabel = document.createElement("div");
+		cartLabel.className = "smallItem";
+		cartLabel.innerHTML = "Your cart is empty. Please add an item before checking out.";
+		document.getElementById("cart").appendChild(cartLabel);
+		//keep check out button disabled, display "your cart is empty" message
+	}
+	
+	else{
+		//loop through each element in products
+		//take name, glaze, number, and picture
+		//create cart item
+		//appendChild to mini cart div
+		//copy above instruction to work on the cart page as well
+		
+		var i;
+		for(i = 0; i<products.length; i++){
+			var bunItem = products[i];
+			var flavor = bunItem.flavor;
+			var glaze = bunItem.glaze;
+			var number = bunItem.number;
+			var pic = bunItem.pic;
+			
+			var cartItem = document.createElement("div");
+			cartItem.className = "smallItem";
+			
+			var cartPic = document.createElement("div");
+			cartPic.className = "itemPic";
+			cartPic.innerHTML = pic;
+			
+			var cartDesc = document.createElement("div");
+			cartDesc.className = "itemDesc";
+			cartDesc.innerHTML = flavor + "<br><br> Glaze: " + glaze + "<br> Quantity: " + number;
+			
+			cartItem.appendChild(cartPic);
+			cartItem.appendChild(cartDesc);
+			
+			document.getElementById("cart").appendChild(cartItem);
+			
+		}	
+		
+	}
+	
+}
+
+function cartLoad() {
+	var count = localStorage.getItem("cartNumber");
+	if(count === null){
+		count = 0;
+	}
+	document.getElementById("lblCartCount").innerHTML = count;
+	
+	var products = JSON.parse(localStorage.getItem("productsList"));
+	
+	if(products === null){		
+		var cartLabel = document.createElement("div");
+		cartLabel.className = "bigItem";
+		cartLabel.innerHTML = "Your cart is currently empty. Please visit the <a href="shop.html">shop</a> to add items.";
+		document.getElementById("cart").appendChild(cartLabel);
+		//keep check out button disabled, display "your cart is empty" message
+	}
+	
+	else{
+		//loop through each element in products
+		//take name, glaze, number, and picture
+		//create cart item
+		//appendChild to mini cart div
+		//copy above instruction to work on the cart page as well
+		
+		var i;
+		for(i = 0; i<products.length; i++){
+			var bunItem = products[i];
+			var flavor = bunItem.flavor;
+			var glaze = bunItem.glaze;
+			var number = bunItem.number;
+			var pic = bunItem.pic;
+			
+			var cartItem = document.createElement("div");
+			cartItem.className = "smallItem";
+			
+			var cartPic = document.createElement("div");
+			cartPic.className = "itemPic";
+			cartPic.innerHTML = pic;
+			
+			var cartDesc = document.createElement("div");
+			cartDesc.className = "itemDesc";
+			cartDesc.innerHTML = flavor + "<br><br> Glaze: " + glaze + "<br> Quantity: " + number;
+			
+			cartItem.appendChild(cartPic);
+			cartItem.appendChild(cartDesc);
+			
+			document.getElementById("cart").appendChild(cartItem);
+			
+		}	
+		
+	}
+	
 }
