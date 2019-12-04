7fdac91e1725a49b1bbf069dbacbb53db5049458
Mel Kerber <mkerber1@andrew.cmu.edu>
2019-12-04 16:13:29 -0500

    JS testing

---
+++ assgn8/script.js
@@ -0,0 +1,10 @@
+function playGulltown() {
+	var bGull = document.getElementById("gullMap").contentDocument;
+	console.log(bGull);
+	
+	//.getElementsByClassName("baratheon");
+	//for(i=0; i<bGull.length; i++){
+	//	bGull[i].setAttribute("stroke", "black");
+	//}
+}
+	
