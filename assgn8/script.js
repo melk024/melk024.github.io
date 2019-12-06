312ccd62c460231d42bf2ed1fc35186955aabae0
Mel Kerber <mkerber1@andrew.cmu.edu>
2019-12-05 21:47:04 -0500

    Use Snapsvg to animate troops on map

--- assgn8/script.js
+++ assgn8/script.js
@@ -6,14 +6,66 @@ function playGulltown() {
 	var play = document.getElementsByClassName("play")[0];
 	play.style.top = "90%";
 	play.style.opacity = "0.0";
+	
+	var map = Snap('#gullMap');
+	
+	var bGull = map.selectAll(".baratheon"); //get array of all troops
+	for(i=0; i<bGull.length; i++){
+		var currentX = bGull[i].attr("cx"); //get current x value
+		var newX = parseInt(currentX)+20; //add 20
+		
+		var currentY = bGull[i].attr("cy"); //get current y value
+		var newY = parseInt(currentY)+10; //add 20
+		
+		bGull[i].animate(
+			{cx: newX,
+			cy: newY},
+			1500); //use Snap to animate
+	}
+	
+	var sGull = map.selectAll(".stark");
+	for(i=0; i<sGull.length; i++){
+		var currentX = sGull[i].attr("cx");
+		var newX = parseInt(currentX)-40;
+		
+		var currentY = sGull[i].attr("cy");
+		var newY = parseInt(currentY)-40;
+		
+		sGull[i].animate(
+			{cx: newX,
+			cy: newY},
+			1500);
+	}
+	
+	var aGull = map.selectAll(".arryn");
+	for(i=0; i<aGull.length; i++){
+		var currentX = aGull[i].attr("cx");
+		var newX = parseInt(currentX)+40;
+		
+		var currentY = aGull[i].attr("cy");
+		var newY = parseInt(currentY)+40;
+		
+		aGull[i].animate(
+			{cx: newX,
+			cy: newY},
+			1500);
+	}
+	
+	var gGull = map.selectAll(".grafton");
+	for(i=0; i<gGull.length; i++){
+		var currentX = gGull[i].attr("cx");
+		var newX = parseInt(currentX)-10;
+		
+		var currentY = gGull[i].attr("cy");
+		var newY = parseInt(currentY)-10;
+		
+		gGull[i].animate(
+			{cx: newX,
+			cy: newY},
+			1500);
+	}
 
-	var bGull = document.getElementById("gullMap").contentDocument.getElementsByClassName("baratheon");
-	anime ({
-		targets: bGull,
-		translateX: {
-			value: '+=45',
-		duration: 2000},
-	});
+	
 	
 	//fill info innerHTML with info about the battle
 	//coming next lol
@@ -24,7 +76,7 @@ function playGulltown() {
 }
 	
 function gulltownStag() {
-	var bGull = document.getElementById("gullMap").contentDocument.getElementsByClassName("baratheon");
+	var bGull = document.getElementById("gullMap").getElementsByClassName("baratheon");
 	for(i=0; i<bGull.length; i++){
 		bGull[i].setAttribute("stroke", "black");
 		bGull[i].setAttribute("stroke-width", "3");
